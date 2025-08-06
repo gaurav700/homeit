8a72ce14b1353ec146aa96d4fa22cfb7d441da92
gaurav700 <jangidg0786@gmail.com>
2025-08-06 01:09:21 -0400

    Adding all external files

---
+++ auth-service/src/main/java/com/homeit/auth_microservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.homeit.auth_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
---
+++ auth-service/src/test/java/com/homeit/auth_microservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeit.auth_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
